98ba02884778313966e614c073432a15bdaef30e
Shelvin Kumar <shalvink42@gmail.com>
2022-10-30 21:13:42 +1300

    V2.1.4
    -Added Test Package (playertest)
    -Added Test Package (DeckTest)

--- BlackJackGame/test/DeckTest.java
+++ BlackJackGame/test/DeckTest.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -7,9 +6,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Bradk
+/*
+ * @author Shelvin Kumar
  */
 public class DeckTest {
 
@@ -31,6 +29,31 @@ public class DeckTest {
     @After
     public void tearDown() {
     }
-    
+
+    /
+     * Test of getDeckCards method, of class Deck.
+     */
+    @Test
+    public void testGetDeckCards() {
+        System.out.println("getDeckCards");
+        Deck testDeck = new Deck();
+        ArrayList<Card> result = testDeck.getDeckCards();
+        assertTrue(result.size() == 52); // Check for all 52 cards
+    }
+
+    /**
+     * Test of switchCards method, of class Deck.
+     */
+    @Test
+    public void testSwitchCards() {
+        System.out.println("switchCards");
+        int i = 1;
+        int j = 11;
+        Deck testDeck = new Deck();
+        Card card1 = testDeck.getDeckCards().get(i);
+        testDeck.switchCards(i, j);
+        Card card2 = testDeck.getDeckCards().get(j);
+        assertSame(card1,card2);
+    }
 
 }
--- BlackJackGame/test/PlayerTest.java
+++ BlackJackGame/test/PlayerTest.java
@@ -7,28 +7,89 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Bradk
+ * @author Shelvin kumar
  */
 public class PlayerTest {
-    
+
     public PlayerTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    
+    /**
+     * Test of getBalance method, of class Player.
+     */
+    @Test
+    public void testGetBalance() {
+        System.out.println("getBalance");
+        Player testPlayer = new Player("test", 512);
+        int expResult = 512;
+        int result = testPlayer.getBalance();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setBalance method, of class Player.
+     */
+    @Test
+    public void testSetBalance() {
+        System.out.println("setBalance");
+        int newBalance = 500;
+        Player testPlayer = new Player("test", 512);
+        testPlayer.setBalance(newBalance);
+        assertEquals(testPlayer.getBalance(), newBalance);
+    }
+
+    /**
+     * Test of getName method, of class Player.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        Player testPlayer = new Player("test", 512);
+        String expResult = "test";
+        String result = testPlayer.getName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setName method, of class Player.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        String name = "testname";
+        Player testPlayer = new Player("notmyname", 512);
+        testPlayer.setName(name);
+        // TODO review the generated test code and remove the default call to fail.
+        assertEquals(name, testPlayer.getName());
+    }
+
+    /**
+     * Test of getMoneyInGame method, of class Player.
+     */
+    @Test
+    public void testGetMoneyInGame() {
+        System.out.println("getMoneyInGame");
+        Player testPlayer = new Player("test", 100);
+        int expResult = 50;
+        testPlayer.setMoneyInGame(expResult);
+        int result = testPlayer.getMoneyInGame();
+        assertEquals(expResult, result);
+    }
+
 }
